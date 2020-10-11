ad3dcee3fc5fe10667be767d2603753fa0fde5be
Adrian Batzill <dev@batzill.com>
2020-10-11 11:04:04 +0200

    support ogn stack 2.8.0

--- ognreader.py
+++ ognreader.py
@@ -4,8 +4,29 @@ import subprocess
 import re
 import os
 import json
-import ogn.parser.telnet_parser
-from ogn.parser.pattern import PATTERN_TELNET_50001
+
+
+# Shamelessly taken from https://github.com/glidernet/python-ogn-client/blob/master/ogn/parser/pattern.py
+# and adapted for OGN 2.8 stack (and reduced a bit for fields we don't need)
+PATTERN_TELNET_50001 = re.compile(r"""
+    (?P<pps_offset>\d\.\d+)sec:(?P<frequency>\d+\.\d+)MHz:\s+
+    (?P<aircraft_type>\d):(?P<address_type>\d):(?P<address>[A-F0-9]{6})\s
+    (?P<timestamp>\d{6}):\s
+    \[\s*(?P<latitude>[+-]\d+\.\d+),\s*(?P<longitude>[+-]\d+\.\d+)\]deg\s*
+    (?P<altitude>\d+)m\s*
+    (?P<climb_rate>[+-]\d+\.\d+)m/s\s*
+    (?P<ground_speed>\d+\.\d+)m/s\s*
+    (?P<track>\d+\.\d+)deg\s*
+    (?P<turn_rate>[+-]\d+\.\d+)deg/s\s*
+    (?P<magic_number>[0-9a-z]+)\s*
+    (?P<gps_status>[0-9x]+)m\s*
+    (?P<channelinfo>\S+)\s*
+    (?P<frequency_offset>[+-]\d+\.\d+)kHz\s*
+    (?P<decode_quality>\d+\.\d+)/(?P<signal_quality>\d+\.\d+)dB/(?P<demodulator_type>\d+)\s+
+    (?P<error_count>\d+)e\s*
+    (?P<distance>\d+\.\d+)km\s*
+    (?P<bearing>\d+\.\d+)deg\s*
+""", re.VERBOSE | re.MULTILINE)
 
 class OgnReader(threading.Thread):
     def __init__(self, callback):
@@ -55,6 +76,7 @@ class OgnReader(threading.Thread):
             search = re.search(PATTERN_TELNET_50001, str(line))
             if search is not None:
                 res = search.groupdict()
+                #print(str(res))
                 if res['address'] in self.ogn_devicedb:
                     res['registration'] = self.ogn_devicedb[res['address']]
                 else:
