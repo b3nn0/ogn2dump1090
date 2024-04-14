e80036baa69727ca57e8b9db91823e78bb066772
Adrian Batzill <dev@batzill.com>
2024-04-14 13:21:46 +0200

    addres_type optional

--- ognreader.py
+++ ognreader.py
@@ -10,7 +10,7 @@ import json
 # and adapted for OGN 2.8 stack (and reduced a bit for fields we don't need)
 PATTERN_TELNET_50001 = re.compile(r"""
     (?P<pps_offset>\d\.\d+)sec:(?P<frequency>\d+\.\d+)MHz:\s+
-    (?P<aircraft_type>.):(?P<address>[A-F0-9]{6})\s
+    (?P<aircraft_type>.):(?P<address_type>\d:)?(?P<address>[A-F0-9]{6})\s
     (?P<timestamp>\d{6}):\s
     \[\s*(?P<latitude>[+-]\d+\.\d+),\s*(?P<longitude>[+-]\d+\.\d+)\]deg\s*
     (?P<altitude>\d+)m\s*
