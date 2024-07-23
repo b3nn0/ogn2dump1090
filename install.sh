4962de73c2a3b33fb367e51a2182f133ab61f13e
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-07-23 16:20:03 +0200

    updating legacy install.sh

--- install.sh
+++ install.sh
@@ -55,14 +55,14 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo blacklist dvb_usb_v2 | sudo tee -a /etc/modprobe.d/rtl-glidernet-blacklist.conf
     echo blacklist rtl8xxxu | sudo tee -a /etc/modprobe.d/rtl-glidernet-blacklist.conf
     
-    # download and unpack version 0.3.0
+    # download and unpack version 0.3.2
     ARCH=$(getconf LONG_BIT)
     DIST=$(lsb_release -r -s)
     if [ "$ARCH" -eq 64 ] && [ "$DIST" -ge 12 ]; then
-        wget http://download.glidernet.org/arm64/rtlsdr-ogn-bin-arm64-0.3.0.tgz
+        wget http://download.glidernet.org/arm64/rtlsdr-ogn-bin-arm64-0.3.2.tgz
     else
-        if [ "$ARCH" -eq 32 ]; then
-            wget http://download.glidernet.org/arm/rtlsdr-ogn-bin-ARM-0.3.0.tgz
+        if [ "$ARCH" -eq 32 ] && [ "$DIST" -ge 11 ]; then
+            wget http://download.glidernet.org/arm/rtlsdr-ogn-bin-ARM-0.3.2.tgz
         else
             echo
             echo "wrong platform for this script, exiting"
