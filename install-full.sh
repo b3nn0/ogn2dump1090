4cc1fcb51413f492e03aced5a2213c63d191637f
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2025-03-27 10:11:05 +0100

    compile librtlsdr from latest osmocom sources

--- install-full.sh
+++ install-full.sh
@@ -11,30 +11,26 @@ sudo apt install debhelper -y
 ARCH=$(getconf LONG_BIT)
 DIST=$(lsb_release -r -s)
 
-# install librtlsdr from http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr
-if [ $ARCH -eq 64 ] && [ "$DIST" -eq 12 ]; then
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_2.0.2-2_arm64.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_2.0.2-2_arm64.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_2.0.2-2_arm64.deb
-fi
-if [ $ARCH -eq 32 ] && [ "$DIST" -eq 12 ]; then # no RTL-SDR Blog V4 dongle support
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_arm64.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_arm64.deb
-fi 
-if [ $ARCH -eq 64 ] && [ "$DIST" -eq 11 ]; then # no RTL-SDR Blog V4 dongle support
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_arm64.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_arm64.deb
-fi
-if [ $ARCH -eq 32 ] && [ "$DIST" -eq 11 ]; then # no RTL-SDR Blog V4 dongle support
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_armhf.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_armhf.deb
-    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_armhf.deb
-fi
+# remove installed librtlsdr
+sudo apt purge ^librtlsdr
+sudo rm -rf /usr/lib/librtlsdr*
+sudo rm -rf /usr/include/rtl-sdr*
+sudo rm -rf /usr/local/lib/librtlsdr*
+sudo rm -rf /usr/local/lib/aarch64-linux-gnu/librtlsdr*
+sudo rm -rf /usr/local/include/rtl-sdr*
+sudo rm -rf /usr/local/include/rtl_*
+sudo rm -rf /usr/local/bin/rtl_*
+
+# compile and install latest librtlsdr from https://github.com/osmocom/rtl-sdr
+cd
+git clone https://github.com/osmocom/rtl-sdr
+cd rtl-sdr
+sudo dpkg-buildpackage -b --no-sign
+cd
 sudo dpkg -i *.deb
 rm -f *.deb
-sudo ldconfig
+rm -f *.buildinfo
+rm -f *.changes
 
 echo blacklist rtl2832 | sudo tee /etc/modprobe.d/rtl-sdr-blacklist.conf
 echo blacklist r820t | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
