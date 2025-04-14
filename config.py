1149accd150ec2e491d90055f7a4b60228232049
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2025-04-14 10:08:56 +0200

    correct some typos

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# Host/POrt where readsb is running with something like --net-sbs-in-port=30008
+# Host/Port where readsb is running with something like --net-sbs-in-port=30008
 sbs_destination_host = "127.0.0.1"
 sbs_destination_port = 30008
 
 
-# Host/Port where ogn-decode telnet listens. Leave empty if yo only want APRS Proxy functionality
+# Host/Port where ogn-decode telnet listens. Leave empty if you only want APRS Proxy functionality
 TELNET_SERVER_HOST = "127.0.0.1"
-TELNET_SERVER_PORT = 50001 
+TELNET_SERVER_PORT = 50001
